c8014c9147e46ce59832c435c7d73f6fe77baa0e
clydetimoteo <20238348@s.ubaguio.edu>
2024-11-29 09:06:35 +0800

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -5,7 +5,7 @@
 $hostname = "localhost";  // Usually 'localhost' if the database is on the same server
 $username = "root";       // Your database username (default is 'root' for local development)
 $password = "";           // Your database password (leave empty if no password set)
-$database = "library_db"; // The name of your database
+$database = "imdb"; // The name of your database
 
 // Establish the connection to the MySQL database
 $db = mysqli_connect($hostname, $username, $password, $database);
